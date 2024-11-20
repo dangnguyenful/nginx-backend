e82bab1e634908e3945b5b570552cba5b9b8160f
Nguyen <dangnguyenful@gmail.com>
2024-11-20 13:29:00 +0700

    test

--- src/main/java/com/example/demo/TodoController.java
+++ src/main/java/com/example/demo/TodoController.java
@@ -11,7 +11,7 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/todos")
+@RequestMapping("/todos")
 public class TodoController {
     private static final Logger logger = LoggerFactory.getLogger(TodoController.class);
 
