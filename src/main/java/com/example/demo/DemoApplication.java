19c554245a37c3232717c3ea39be5b585c8afa0b
Nguyen <dangnguyenful@gmail.com>
2024-11-22 18:40:32 +0700

    Update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.simple.SimpleMeterRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +9,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		MeterRegistry registry = new SimpleMeterRegistry();
+
+		MetricsExample metrics = new MetricsExample(registry);
+
+		// Cập nhật số lượng thread đang hoạt động
+		metrics.updateActiveThreads(10);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
+
