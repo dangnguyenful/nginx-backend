950880b750654d490c5b56c2b3a0ab08e87ce4dc
Nguyen <dangnguyenful@gmail.com>
2024-11-22 18:24:55 +0700

    Update

--- src/main/java/com/example/demo/MonitoringConfig.java
+++ src/main/java/com/example/demo/MonitoringConfig.java
@@ -1,5 +1,8 @@
+package com.example.demo;
+
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
